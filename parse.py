d26f3ff44fc396368e4a818aa0fda759d0db93dd
Angel Gonzalo <angoru@gmail.com>
2020-03-17 23:40:26 +0100

    creates js files in new directory

--- parse.py
+++ parse.py
@@ -1,16 +1,23 @@
 import json
+from datetime import datetime
+import os
 
 with open('tok.json', 'r') as f:
     tok_json = json.load(f)
 
 items = tok_json.get('item')
 
+# create a directory to put all js files
+parse_dir_name = 'parse_' + datetime.now().strftime("%s")
+os.mkdir(parse_dir_name)
+
+
 for item in items:
     event = item.get('event')
     if event:
         for data in event:
             script = data.get('script')
-            f = open(item["name"] + ".js","w")
+            f = open(os.path.join(parse_dir_name, item["name"] + ".js"),"w")
             for line in script['exec']:
                 f.write(line + "\n")
             f.close()
